27696b0526fafca530798b9a8e675871f4222282
mikota <lovrogrgic0@gmail.com>
2024-01-17 07:48:18 +0100

    fix u_ioctl via upstream test/ioctl.c

--- lab2b/u_ioctl.cpp
+++ lab2b/u_ioctl.cpp
@@ -4,16 +4,8 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include <iostream>
+#include "config.h"
 
-/* for ioctl */
-#define SHOFER_IOCTL_TYPE	0x8A /* type, unused by https://www.kernel.org/doc/Documentation/ioctl/ioctl-number.txt */
-#define SHOFER_IOCTL_NR		1 /* serial number */
-#define SHOFER_IOCTL_COPY	1 /* command */
-
-struct shofer_ioctl {
-	unsigned int command;
-	unsigned int count;
-};
 
 
 int main() {
@@ -24,10 +16,18 @@ int main() {
     }
 
     struct shofer_ioctl cmd;
+    unsigned long request;
+    request = _IOC(_IOC_READ, SHOFER_IOCTL_TYPE, SHOFER_IOCTL_NR, sizeof(cmd));
     cmd.command = SHOFER_IOCTL_COPY;
     cmd.count = 1;
-    ioctl(fd, SHOFER_IOCTL_TYPE | SHOFER_IOCTL_NR, &cmd);
-
+    
+    int cnt = ioctl(fd, request, (unsigned long)&cmd);
+    if (cnt == -1) {
+        std::cout << "Error ioctl" << std::endl;
+        close(fd);
+        exit(1);
+    }
+    std::cout << "copied " << cnt << " bytes" << std::endl;
     close(fd);
     return 0;
 }
